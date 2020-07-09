3b19ae2a73a4ca7c9dc43545d1bd883465e4d18f
Donghui-Lee <ehdgml3206@naver.com>
2020-07-09 23:07:19 +0900

    style-loader, css-loader, file-loader, url-loader 적용

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require("path");
-const mode = process.env.NODE_ENV || "development";
+const path = require('path');
+const mode = process.env.NODE_ENV || 'development';
+const PRODUCTION = 'production';
+const DEVLEOPMENT = 'development';
 
 // 빌드용 html 파일을 자동 생성
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode,
   entry: {
-    main: path.resolve(__dirname, "src", "index.tsx"),
+    main: path.resolve(__dirname, 'src', 'index.tsx'),
   },
   // 빌드 결과물 dist/main.js 에 위치
   output: {
-    filename: "[name].js",
-    path: __dirname + "/dist",
+    filename: '[name].js',
+    path: __dirname + '/dist',
   },
   resolve: {
     // 파일 확장자 처리
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
@@ -25,18 +27,45 @@ module.exports = {
       {
         test: /\.(ts|tsx|js)$/,
         exclude: /node_modules/,
-        use: "ts-loader",
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader',
+        options: {
+          publicPath: mode === PRODUCTION ? '/dist/' : '/', // 파일 로더가 처리하는 파일을 모듈로 사용했을 때 경로 앞에 추가하는 경로
+          name: 'images/[name].[ext]?[hash]', // 파일 로더가 아웃풋에 파일을 복사할 때 사용하는 파일이름, [원본파일].[확장자명][해쉬값]
+          limit: 20000, // 20KB, 파일을 처리할 때의 크기 제한, 7KB 미만일 경우 url-loader 가처리, 이상일 경우 file-loader 가 처리
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          '@svgr/webpack',
+          {
+            loader: 'url-loader',
+            options: {
+              publicPath: mode === PRODUCTION ? '/dist/' : '/', // 파일 로더가 처리하는 파일을 모듈로 사용했을 때 경로 앞에 추가하는 경로
+              name: 'images/[name].[ext]?[hash]', // 파일 로더가 아웃풋에 파일을 복사할 때 사용하는 파일이름, [원본파일].[확장자명][해쉬값]
+              limit: 20000, // 20KB, 파일을 처리할 때의 크기 제한, 7KB 미만일 경우 url-loader 가처리, 이상일 경우 file-loader 가 처리
+            },
+          },
+        ],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   devServer: {
     port: 3001,
-    contentBase: __dirname + "/dist/",
-    host: "localhost",
+    contentBase: __dirname + '/dist/',
+    host: 'localhost',
   },
 };
