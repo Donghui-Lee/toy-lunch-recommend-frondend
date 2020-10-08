cd6021c3acf6b843560c6fec93f3614c8e6c07e7
dhlee <dhlee@emro.co..kr>
2020-10-08 15:08:29 +0900

    로그인/회원가입FORM적용

--- src/components/auth/AuthForm/AuthFormContainer.tsx
+++ src/components/auth/AuthForm/AuthFormContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AuthFormPresenter from './AuthFormPresenter';
 // import { LOGIN_REQUEST } from '../../../modules/auth/login';
 
@@ -6,21 +6,17 @@ interface AuthFormProps {
     type: string;
 }
 
+interface AuthInput {
+    username: string;
+    password: string;
+    passwordConfirm?: string;
+}
+
 export default function AuthFormContainer({ type }: AuthFormProps) {
-    const [form, setForm] = useState({ username: '', password: '' });
     // Submit Event Handler
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    const onSubmit = (data: AuthInput) => {
         debugger;
     };
-
-    const validateForm = () => {};
-
-    // Chnage Event Handler
-    // const onChange = (e) => {
-    //     const { value, name } = e.target;
-    //     debugger;
-    // };
 
     return (
         <AuthFormPresenter type={type} onSubmit={onSubmit}></AuthFormPresenter>
